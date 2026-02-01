63237decb192da62f1dc5f26e097eb58b150dbe6
unknown <sivareddy9017@gmail.com>
2026-02-01 10:30:28 +0530

    Conv1 added

--- maha.sh
+++ maha.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-echo "Nandyala Reddy"
-
-echo "Maha:: Hello Nandyala how are you?"
-echo "Nandyala:: hello Maha im good thanks for asking how about you?"
-
-echo "Ragada"
+echo "1,2,3,4,5,6"
